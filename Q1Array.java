1e6a94cf6581d88543699c6cb43ab06f1a521dc2
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2025-02-20 19:46:43 +0530

    Update Q1Array.java

--- Q1Array.java
+++ Q1Array.java
@@ -1,27 +1,27 @@
 // second largest no.
-// import java.util.Arrays;
-// class Q1Array{
+import java.util.Arrays;
+class Q1Array{
 
-//     public static int reverse(int arr[]){
+    public static int reverse(int arr[]){
 
-//         Arrays.sort(arr);
+        Arrays.sort(arr);
 
-//         int n = arr.length;
+        int n = arr.length;
 
-//         for(int i=n-2;i>=0;i--)
-//         {
-//             if(arr[i] != arr[n-1])
-//             {return arr[i];}
-//         }
-//         return -1;
-//     }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i] != arr[n-1])
+            {return arr[i];}
+        }
+        return -1;
+    }
 
-//     public static void main(String[] args) {
+    public static void main(String[] args) {
         
-//         int arr[] = {55,4,6,7,99};
-//         System.out.println(reverse(arr));
-//     }
-// }
+        int arr[] = {55,4,6,7,99};
+        System.out.println(reverse(arr));
+    }
+}
 // TC:O(n logn)
 
 //second largest no. with user input
@@ -61,57 +61,57 @@ class Q1Array{
 
 
 //2nd largest no. optimized approach 
-// class Q1Array{
-//     public static int secondLargestNum(int arr[]){
-//         int largest = Integer.MIN_VALUE, secondlargest=Integer.MIN_VALUE;
+class Q1Array{
+    public static int secondLargestNum(int arr[]){
+        int largest = Integer.MIN_VALUE, secondlargest=Integer.MIN_VALUE;
 
-//         for(int num: arr){
-//             if(num > largest){
-//                 secondlargest = largest;
-//                 largest = num;
-//             }
-//             else if(num != largest && num > secondlargest ){
-//                 secondlargest = num;
-//             }
-//         }
+        for(int num: arr){
+            if(num > largest){
+                secondlargest = largest;
+                largest = num;
+            }
+            else if(num != largest && num > secondlargest ){
+                secondlargest = num;
+            }
+        }
 
-//         return (secondlargest == Integer.MIN_VALUE ? -1 : secondlargest);
-//     }
+        return (secondlargest == Integer.MIN_VALUE ? -1 : secondlargest);
+    }
 
-//     public static void main(String[] args) {
-//         int arr[] = {51,5,8,66,7};
-//         System.out.println(secondLargestNum(arr));
-//     }
-// }
+    public static void main(String[] args) {
+        int arr[] = {51,5,8,66,7};
+        System.out.println(secondLargestNum(arr));
+    }
+}
 
-//TC: O(n)
+TC: O(n)
 
 
 
 //3rd largest no.
 
-// import java.util.Arrays;
+import java.util.Arrays;
 
-// class Q1Array{
+class Q1Array{
 
-//     static int reverse(int arr[]){
-//         int n = arr.length;
+    static int reverse(int arr[]){
+        int n = arr.length;
 
-//         Arrays.sort(arr);
+        Arrays.sort(arr);
 
-//        for (int i=n-2;i>=0;i--){
-//             if(arr[i] != arr[n-1]){
-//                 if(arr[i] != arr[n-2])
-//                 {return arr[i];}
-//             }
-//        }
-//        return -1;
-//     }
+       for (int i=n-2;i>=0;i--){
+            if(arr[i] != arr[n-1]){
+                if(arr[i] != arr[n-2])
+                {return arr[i];}
+            }
+       }
+       return -1;
+    }
 
-//     public static void main(String[] args) {
+    public static void main(String[] args) {
         
-//         int arr[] = {10,55,33,4,7};
-//         System.out.println(reverse(arr)); 
-//     }
+        int arr[] = {10,55,33,4,7};
+        System.out.println(reverse(arr)); 
+    }
 
-// }
+}
