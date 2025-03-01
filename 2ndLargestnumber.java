e43eb4f619240b263cf938bc36cd9cc3f4295fd3
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2025-03-01 23:11:09 +0530

    Update 2ndLargestnumber.java

--- 2ndLargestnumber.java
+++ 2ndLargestnumber.java
@@ -86,7 +86,37 @@ class Q1Array{
 
 TC: O(n)
 
-
+//Good approach for 2nd largest element finding
+import java.util.*;
+public class Main {
+    public static int secondLargest(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        
+        if(arr == null || arr.length < 2 ){ throw new IllegalArgumentException("Array must have atleast 2 elements");}
+        for(int num: arr){
+            if(num > max){ max = num;}
+        }
+        for(int num: arr){
+            if(num > secondMax && num < max ) {secondMax = num;}
+        }        
+        if(secondMax == Integer.MIN_VALUE){ throw new RuntimeException("Must must not be empty"); }   
+        return secondMax;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the array elements: ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The secondMax is: "+secondLargest(arr));
+    }
+}
 
 //3rd largest no.
 
