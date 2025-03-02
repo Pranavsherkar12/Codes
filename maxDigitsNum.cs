443e8164e71699285793140a6461544422803fb5
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2025-03-02 20:26:31 +0530

    Update maxDigitsNum.cs

--- maxDigitsNum.cs
+++ maxDigitsNum.cs
@@ -21,3 +21,43 @@ class Program
         Console.WriteLine("Maximum digit number is: " + MaxDigitNum(num));
     }
 }
+
+
+//2nd approach
+using System;
+
+public class HelloWorld
+{
+    
+    static int maxDigits(int n){
+        
+        char[] digits = n.ToString().ToCharArray();
+        int length = digits.Length;
+        
+        for(int i=0 ; i<length-1 ;i++){
+            
+            int maxIndex = i;
+            for(int j= i+1; j<length; j++){
+                if(digits[j] > digits[maxIndex]){
+                    maxIndex = j;
+                }
+            }
+            
+            char temp = digits[i];
+            digits[i] = digits[maxIndex];
+            digits[maxIndex] = temp;
+            
+        }
+        
+        int res = int.Parse(new string(digits));
+        return res;
+        
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter number: ");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine(maxDigits(num));
+    }
+}
